8e1c118f9faff6a76c049c965cba5cd1a5512881
Kyle <kyleratti@users.noreply.github.com>
2017-11-23 14:01:15 -0500

    Added ADLDAP configs

--- config/app.php
+++ config/app.php
@@ -177,6 +177,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * LDAP
+         */
+        Adldap\Laravel\AdldapServiceProvider::class,
+
     ],
 
     /*
@@ -225,6 +230,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Adldap' => Adldap\Laravel\Facades\Adldap::class,
 
     ],
 
