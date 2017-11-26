baade4d95d3227f938ade350994af1796a9360f8
Kyle <kyleratti@users.noreply.github.com>
2017-11-26 13:50:17 -0500

    Added code to AuthController

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -7,5 +7,23 @@ use App\Http\Controllers\Controller;
 
 class AuthController extends Controller
 {
-    //
+    /**
+     * Redirect the user to the Graph authentication page
+     * 
+     * @return Response
+     */
+    public function redirectToProvider()
+    {
+        return Socialize::with('graph')->redirect();
+    }
+
+    /**
+     * Obtain the user information from graph
+     * 
+     * @return Response
+     */
+    public function handleProviderCallback(Request $objRequest)
+    {
+        $objUser = Socialize::with('graph')->user();
+    }
 }
