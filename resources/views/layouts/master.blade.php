4cf9bce972f51cdc5f2ea4accdf62e221881b03a
Kyle <kyleratti@users.noreply.github.com>
2017-11-26 14:15:25 -0500

    Added basic template

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>@yield("title") - CoffeeBreak</title>
+</head>
+<body>
+    <div class="header">
+        <h1>CoffeeBreak</h1>
+    </div>
+
+    <div class="container">
+        <div class="content">
+            @yield("content")
+        </div>
+    </div>
+</body>
+</html>
