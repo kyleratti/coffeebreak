d483db494c8099f58c5b3d89d126c84308fa5a5d
Kyle <kyleratti@users.noreply.github.com>
2017-11-26 21:38:31 -0500

    Updated layout for displaying drinks

--- resources/views/menu/show.blade.php
+++ resources/views/menu/show.blade.php
@@ -6,6 +6,8 @@
     <p>Please browse our vast selection of drinks below. When you're ready, just click the <b>Place Order</b> button.</p>
 
     <div class="drinks">
-        @each('menu.item', $objDrinks, 'objDrink', 'menu.empty')
+        <div class="card-deck">
+            @each('menu.item', $objDrinks, 'objDrink', 'menu.empty')
+        </div>
     </div>
 @endsection
