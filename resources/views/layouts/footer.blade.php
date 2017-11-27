adaba074f8c47d70329a29c0da50b33dda63f508
Kyle <kyleratti@users.noreply.github.com>
2017-11-26 23:45:46 -0500

    Added link to this GitHub repo

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="container footer">
     <div class="fixed-bottom text-right text-muted">
-        made with <span class="text-danger">&hearts;</span> for <a href="http://westminsteramerican.com">Westminster American Insurance Company</a>
+        made with <span class="text-danger">&hearts;</span> for <a href="http://westminsteramerican.com">Westminster American Insurance Company</a>. <small><a href="https://github.com/kyleratti/coffeebreak">source</a></small>.
     </div>
 </div>
