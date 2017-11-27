9360531d8bce1f6d1ed47c354a37a2bddc9938fa
Kyle <kyleratti@users.noreply.github.com>
2017-11-26 20:03:25 -0500

    Updated footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="container footer">
-    <div class="fixed-bottom text-right">
-        Made with <span class="text-danger">&hearts;</span> for <a href="http://westminsteramerican.com">Westminster American Insurance Company</a>.
+    <div class="fixed-bottom text-right text-muted">
+        made with <span class="text-danger">&hearts;</span> for <a href="http://westminsteramerican.com">Westminster American Insurance Company</a>
     </div>
 </div>
