ac8dab0809791292e41168f5bf330a56ed078e5a
Kyle <kyleratti@users.noreply.github.com>
2017-11-26 19:36:30 -0500

    Added footer template

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<div class="container footer">
+    <div class="fixed-bottom text-right">
+        Made with <span class="text-danger">&hearts;</span> for <a href="http://westminsteramerican.com">Westminster American Insurance Company</a>.
+    </div>
+</div>
