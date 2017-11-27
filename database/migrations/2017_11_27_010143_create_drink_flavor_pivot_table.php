a952fee641e320f7de30aa39841ad79ac967d87a
Kyle <kyleratti@users.noreply.github.com>
2017-11-26 21:39:09 -0500

    Fixed incorrect class name

--- database/migrations/2017_11_27_010143_create_drink_flavor_pivot_table.php
+++ database/migrations/2017_11_27_010143_create_drink_flavor_pivot_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class App\ extends Migration
+class CreateDrinkFlavorPivotTable extends Migration
 {
     /**
      * Run the migrations.
