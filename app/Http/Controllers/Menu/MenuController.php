331983ea9d5c894b4e5b900f0db243ebf44523db
Kyle <kyleratti@users.noreply.github.com>
2017-11-26 21:40:58 -0500

    Updated menu controller

--- app/Http/Controllers/Menu/MenuController.php
+++ app/Http/Controllers/Menu/MenuController.php
@@ -5,8 +5,20 @@ namespace App\Http\Controllers\Menu;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Drink\Drink;
+
 class MenuController extends Controller
 {
+    /**
+     * Instantiate a new order controller instance
+     * 
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Show the menu
      * 
@@ -14,6 +26,15 @@ class MenuController extends Controller
      */
     public function show()
     {
-        return view('menu.show');
+        $arrDrinksInStock = [];
+
+        foreach(Drink::where('is_in_stock', 1)->get() as $objDrink) {
+            if($objDrink->isInStock())
+                $arrDrinksInStock[] = $objDrink;
+        }
+
+        return view('menu.show', [
+            'objDrinks' => $arrDrinksInStock,
+        ]);
     }
 }
