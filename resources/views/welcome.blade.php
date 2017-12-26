cb4d22b327d5be679ed3ec0ca3681e3b993e09e0
Kyle Ratti <kyle.ratti@outlook.com>
2017-12-26 10:27:57 -0500

    Updated welcome message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <h1 class="display-1">Welcome</h1>
 
-    <p>I understand you have a lot of choices in ordering coffee at work today, so I just wanted to take a moment and thank you for choosing CoffeeBreak. It means a lot. This is a <small>very</small> quickly thrown together web page that tries to provide centralized espresso beverage ordering and at-desk notifications.</p>
+    <p>I understand you have a lot of choices in ordering espresso-based beverages at work today, so I just wanted to take a moment and thank you for choosing CoffeeBreak. It means a lot. This is a small web page thrown together <small>very</small> quickly that tries to provide centralized espresso beverage ordering and at-desk notifications.</p>
 
     <p>I make absolutely no claims to the reliability of this website. If it messes up and you don't get your coffee, please don't yell at me!</p>
 @endsection
