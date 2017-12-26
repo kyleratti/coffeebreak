e0f831de53071ad3ce85d571cf219ba628539de5
Kyle Ratti <kyle.ratti@outlook.com>
2017-12-26 11:29:57 -0500

    Added drinks

--- database/migrations/2017_12_26_153426_create_drinks_flavors.php
+++ database/migrations/2017_12_26_153426_create_drinks_flavors.php
@@ -23,6 +23,11 @@ class CreateDrinksFlavors extends Migration
             'is_in_stock' => true,
         ]);
 
+        $objCaramel = Flavor::create([
+            'name' => 'Caramel',
+            'is_in_stock' => true,
+        ]);
+
         $objPeach = Flavor::create([
             'name' => 'Peach',
             'is_in_stock' => true,
@@ -30,6 +35,11 @@ class CreateDrinksFlavors extends Migration
 
         $objRaspberry = Flavor::create([
             'name' => 'Raspberry',
+            'is_in_stock' => true,
+        ]);
+
+        $objChocolate = Flavor::create([
+            'name' => 'Chocolate Mocha',
             'is_in_stock' => true,
         ]);
 
@@ -49,24 +59,43 @@ class CreateDrinksFlavors extends Migration
             'name' => 'Latte',
             'description' => 'A classic latte consisting of steamed milk and espresso shots',
             'shots' => 2,
-            'is_in_stock' => true,
         ]);
 
         $objVanillaLatte = Drink::create([
             'name' => 'Vanilla Latte',
             'description' => 'Your standard latte with some vanilla flavoring',
             'shots' => 2,
-            'is_in_stock' => true,
         ]);
         $objVanillaLatte->flavors()->attach($objVanilla->id);
+
+        $objCaramelLatte = Drink::create([
+            'Caramel Latte',
+            'description' => 'A latte with caramel flavoring',
+            'shots' => 2,
+        ]);
+        $objCaramelLatte->flavors()->attach($objCaramel->id);
 
         $objRaspberryLatte = Drink::create([
             'name' => 'Raspberry Latte',
             'description' => 'A latte with raspberry flavoring',
             'shots' => 2,
-            'is_in_stock' => true,
         ]);
         $objRaspberryLatte->flavors()->attach($objRaspberry->id);
+
+        $objMocha = Drink::create([
+            'name' => 'Mocha',
+            'description' => 'A chocolate flavored drink with espresso shots and topped with whipped cream',
+            'shots' => 2,
+        ]);
+        $objMocha->flavors()->attach($objChocolate->id);
+
+        $objHotChocolate = Drink::create([
+            'name' => 'Hot Chocolate',
+            'description' => 'It\'s hot chocolate! Come on now! Vanilla and chocolate mixed with steamed milk and topped with whipped cream',
+            'shots' => 0,
+        ]);
+        $objHotChocolate->flavors()->attach($objChocolate->id);
+        $objHotChocolate->flavors()->attach($objVanilla->id);
     }
 
     /**
