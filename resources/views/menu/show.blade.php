d2f12972c906ff747e2a7af1203c10fcaf27ed7c
Kyle <kyleratti@users.noreply.github.com>
2017-12-27 13:37:18 -0500

    Changed layout

--- resources/views/menu/show.blade.php
+++ resources/views/menu/show.blade.php
@@ -4,18 +4,22 @@
 
 @section('content')
     @if(!Setting::get('accepting_orders', false))
-        <div class="alert alert-danger">
-            <b>New orders are currently not being accepted!</b>
-        </div>
-
-        <p>You can browse our menu below, but you won't be able to place an order.</p>
+        <p>You can browse the menu below, but you won't be able to place an order.</p>
     @else
-        <p>Please browse our vast selection of drinks below. When you're ready, just click the <b>Place Order</b> button.</p>
+        <p>Please browse our <i>vast</i> selection of drinks below. When you're ready, just click the <b>Place Order</b> button.</p>
     @endif
 
     <div class="drinks">
-        <div class="card-group">
-            @each('menu.item', $objDrinks, 'objDrink', 'menu.empty')
-        </div>
+        @foreach($objDrinks as $objDrink)
+            @if($loop->index % 3 == 0)
+                <div class="card-deck">
+            @endif
+
+            @include('menu.item', ['objDrink' => $objDrink])
+
+            @if(($loop->index + 1) % 3 == 0)
+                </div>
+            @endif
+        @endforeach
     </div>
 @endsection
