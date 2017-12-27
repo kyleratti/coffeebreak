d72999f9d81458c4edd747f2428df50842153cfd
Kyle <kyleratti@users.noreply.github.com>
2017-12-27 13:38:03 -0500

    Fixed layout

--- resources/views/recipe/view/flavor/item.blade.php
+++ resources/views/recipe/view/flavor/item.blade.php
@@ -1,6 +1,6 @@
-<a href="#" class="list-group-item list-group-item-action flex-column align-items-start list-group-item-{{ $objFlavor->is_in_stock ? 'success' : 'danger'}}">
+<a href="{{ route('admin.settings.availability.flavor.toggle', $objFlavor->id) }}" class="list-group-item list-group-item-action flex-column align-items-start list-group-item-{{ $objFlavor->is_in_stock ? 'success' : 'danger'}}">
     <div class="d-flex w-25 justify-content-between">
-        <h5 class="mb-1">{{ $objFlavor->name }}</h5>
+        <p class="mb-1">{{ $objFlavor->name }}</p>
     </div>
 </a>
     
--- resources/views/recipe/view/milk/item.blade.php
+++ resources/views/recipe/view/milk/item.blade.php
@@ -1,5 +1,5 @@
-<a href="#" class="list-group-item list-group-item-action flex-column align-items-start list-group-item-{{ $objMilk->is_in_stock ? 'success' : 'danger'}}">
+<a href="{{ route('admin.settings.availability.milk.toggle', $objMilk->id) }}" class="list-group-item list-group-item-action flex-column align-items-start list-group-item-{{ $objMilk->is_in_stock ? 'success' : 'danger'}}">
     <div class="d-flex w-25 justify-content-between">
-        <h5 class="mb-1">{{ $objMilk->name }}</h5>
+        <p class="mb-1">{{ $objMilk->name }}</p>
     </div>
 </a>
