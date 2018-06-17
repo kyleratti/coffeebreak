f5ec1097143bb0e8d2a2b5826b06f7768e5b4e5e
Kyle <kyleratti@users.noreply.github.com>
2018-06-17 15:28:05 -0400

    Updated login page

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -3,9 +3,11 @@
 @section('title', 'Login')
 
 @section('content')
-    <p>In an effort to simplify the number of logins you have, this website doesn't require you to create a new account.</p>
-    
-    <p>Instead, you'll be sent over to Microsoft's website to sign in with your Office 365 e-mail account, and then automatically sent back here once you're signed in.</p>
+    <h1 class="display-1">Log in</h1>
+
+    <p><b>CoffeeBreak</b> uses your work Office 365 account to sign in.</p>
+
+    <p>You'll be sent to Microsoft for your credentials and returned here when you're finished.</p>
 
     <div class="graph-signin">
         <a href="{{ route('user.login.go') }}"><img src="https://docs.microsoft.com/en-us/azure/active-directory/develop/media/active-directory-branding-guidelines/sign-in-with-microsoft-light.png" alt="Sign in with Microsoft"/></a>
