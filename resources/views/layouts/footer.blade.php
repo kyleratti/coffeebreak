a1a8f43bbcf6a497da179d39e5afdf3a1b94273f
Kyle <kyleratti@users.noreply.github.com>
2018-06-17 15:39:34 -0400

    Changed wording on footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="container footer">
     <div class="fixed-bottom text-right text-muted">
-        made with <span class="text-danger">&hearts;</span> for <a href="http://westminsteramerican.com">Westminster American Insurance Company</a>. <small><a href="https://github.com/kyleratti/coffeebreak"><img src="/img/GitHub-Mark-32px.png" class="github" alt="GitHub" target="_blank"/></a></small>
+        crafted with <span class="text-danger">&hearts;</span> for <a href="http://westminsteramerican.com">Westminster American</a>. <small><a href="https://github.com/kyleratti/coffeebreak"><img src="/img/GitHub-Mark-32px.png" class="github" alt="GitHub" target="_blank"/></a></small>
     </div>
 </div>
