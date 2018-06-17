6b903debc1bafe600c14381635573bac8d1cc0ba
Kyle <kyleratti@users.noreply.github.com>
2018-06-17 14:26:03 -0400

    Removed Laravel project

--- resources/views/recipe/view/milk/empty.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="alert alert-danger">
-    <strong>Huh.</strong> There's no milk registered with this application. Which means something is pretty broken. Please report this.
-</div>
-    
